132fff053d0c3efc7b31f76cbbe07c2df8d0bc0f
Neyas Anand <nsidhanandan@enphaseenergy.com>
2022-08-01 23:38:12 +0530

    Adding Dockerfiles

--- api-gateway/src/test/java/com/neyas/microservices/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/neyas/microservices/ApiGatewayApplicationTests.java
@@ -1,13 +1,13 @@
 package com.neyas.microservices;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
--- cloud-config-server/src/test/java/com/neyas/cloudconfigserver/CloudConfigServerApplicationTests.java
+++ cloud-config-server/src/test/java/com/neyas/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -1,13 +1,13 @@
 package com.neyas.cloudconfigserver;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CloudConfigServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
